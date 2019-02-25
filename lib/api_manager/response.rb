f7dda254026243e10e8e05aa6d82a2fd57cb88e6
Edward McCaffrey <edward.mccaffrey@sealink.com.au>
2019-02-25 12:18:37 +1030

    Minor refactor

--- lib/api_manager/response.rb
+++ lib/api_manager/response.rb
@@ -2,7 +2,7 @@ module APIManager
   module Response
     def self.build(raw_response, options)
       klass = APIManager::Response::Base.for_type(options[:content_type].to_s.camelcase)
-      raise "Unable to determine API response type" if !klass.present?
+      raise "Unable to determine API response type" unless klass.present?
       klass.new(raw_response, options)
     end
   end
